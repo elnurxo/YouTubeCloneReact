94e7f1aa4626ab7691d193b9508704ef5e6e4dc1
elnurxo <elnurykh@code.edu.az>
2022-09-18 14:16:42 +0400

    too many bug fixes2

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,9 +2,8 @@ import { VideoCard, ChannelCard } from './';
 import {Stack, Box} from '@mui/material';
 
 function Videos({videos}) {
-    console.log('videos: ',videos);
   return (
-   <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={1}>
+   <Stack direction='row' flexWrap='wrap' justifyContent='flex-start' gap={1}>
         {videos?.map((item, id) => (
         <Box key={id}>
           {item.id.videoId && <VideoCard video={item} /> }
