475da662940785c8045e0ce84744e4e70c9f7231
elnurxo <elnurykh@code.edu.az>
2022-09-18 09:53:19 +0400

    sidebar and navbar halfway done

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export {default as Feed } from './Feed';
 export {default as VideoDetail } from './VideoDetail';
 export {default as ChannelDetail } from './ChannelDetail';
 export {default as SearchFeed } from './SearchFeed';
+export {default as SearchBae } from './SearchBar';
+export {default as SideBar } from './SideBar';
