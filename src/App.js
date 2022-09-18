b1bc2d750a15444c9e05d3daab276089508819be
elnurxo <elnurykh@code.edu.az>
2022-09-18 09:09:04 +0400

    folder structure done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+//Components Importing
+import {Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed } from './components';
 
 function App() {
   return (
-    <>
-      <h1>YOUTUBE 1</h1>
-    </>
+    <BrowserRouter>
+      <Box sx={{backgroundColor:"black"}}>
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Feed/>}/>
+            <Route path="/video/:id" element={<VideoDetail/>}/>
+            <Route path="/channel/:id" element={<ChannelDetail/>}/>
+            <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
+        </Routes>
+        {/* Footer */}
+      </Box>
+    </BrowserRouter>
   );
 }
 
