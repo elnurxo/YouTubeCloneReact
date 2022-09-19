c185e4c60a73ea5e68d67a5fe669bf578e1d708b
elnurxo <elnurykh@code.edu.az>
2022-09-19 13:24:15 +0400

    bug fix, default channel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -35,8 +35,8 @@ export const categories = [
 ];
 
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoChannelUrl = '/channel/UC0WP5P-ufpRfjbNrmOWwLBQ';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
+export const demoChannelTitle = 'The Weeknd';
 export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
